ebb8aeaaa5c9b438c8d75c27687000f78bf36bd7
bmassengill <beverly.massengill@metova.com>
2019-11-12 12:47:12 -0600

    Set up XIB-based view hierarchy

--- Paradigms/View Controllers/HomeViewController.swift
+++ Paradigms/View Controllers/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  Paradigms
 //
 //  Created by Beverly Massengill on 11/12/19.
@@ -8,13 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
