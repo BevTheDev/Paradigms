16c1070f7ba54f044e5efe1865622af2a16bc926
bmassengill <beverly.massengill@metova.com>
2019-12-18 14:39:00 -0600

    Specify timezone for tests

--- ParadigmsTests/MockObjects/MockEnvironment.swift
+++ ParadigmsTests/MockObjects/MockEnvironment.swift
@@ -14,6 +14,6 @@ import XCTest
 extension Environment {
     
     static let mock = Environment(
-        date: Date(year: 2019, month: 11, day: 12, hour: 2, minute: 45)
+        date: Date(year: 2019, month: 11, day: 12, hour: 2, minute: 45, second: 0, millisecond: 0, timeZone: TimeZone.init(abbreviation: "CST"))
     )
 }
